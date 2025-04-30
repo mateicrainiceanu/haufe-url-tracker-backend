89be917650f16da9bd1f690eebc83492e421c1b9
mateicrainiceanu <matei.crainiceanu@gmail.com>
2025-04-30 13:18:39 +0300

    Status updated test for deploy

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,7 @@ app.use(cors());
 app.use(rootRouter);
 
 app.get('/api/v1/status', async (req, res) => {
-  res.status(200).json({ status: 'ok' });
+  res.status(200).json({ status: 'ok', code: 2 });
 });
 
 app.use("/api/v1/", registerRoute, loginRoute);
