21e57e4f64b6602fd2f681899f823c6bf3dcca17
mateicrainiceanu <matei.crainiceanu@gmail.com>
2025-05-06 17:00:12 +0300

    File logging .env property

--- src/config/global.ts
+++ src/config/global.ts
@@ -2,5 +2,6 @@ require("dotenv").config();
 
 export const jwtsecret = process.env.JWT_SECRET;
 export const logLevel = process.env.LOG_LEVEL || "info";
-export const fileLogLever = process.env.FILE_LOG_LEVEL || "info";
+export const fileLogLevel = process.env.FILE_LOG_LEVEL || "info";
+export const enableFileLogging = process.env.FILE_LOGGING === "true";
 export const destinationLogFile = "./logs/" + (process.env.DESTINATION_LOG_FILE || "app") + `-${new Date(Date.now()).toISOString()}` + ".log";
--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,38 +1,43 @@
 import pino, { multistream } from 'pino';
-import { fileLogLever, logLevel } from './global';
+import {enableFileLogging, fileLogLevel, logLevel} from './global';
 import { destinationLogFile } from './global';
 import fs from 'fs';
 import path from 'path';
 
-// const logDirectory = path.dirname(destinationLogFile);
-// if (!fs.existsSync(logDirectory)) {
-//     fs.mkdirSync(logDirectory, { recursive: true });
-// }
+const logDirectory = path.dirname(destinationLogFile);
+if (!fs.existsSync(logDirectory)) {
+    fs.mkdirSync(logDirectory, { recursive: true });
+}
 
-// if (!fs.existsSync(destinationLogFile)) {
-//     fs.writeFileSync(destinationLogFile, '', { flag: 'w' });
-// }
-// const stream = fs.createWriteStream(destinationLogFile, { flags: 'a' });
+if (!fs.existsSync(destinationLogFile)) {
+    fs.writeFileSync(destinationLogFile, '', { flag: 'w' });
+}
+const stream = fs.createWriteStream(destinationLogFile, { flags: 'a' });
+
+stream.close();
+
+const targets: any[] = [ {
+    level: logLevel,
+    target: 'pino-pretty',
+    options: {
+        colorize: true,
+        translateTime: 'SYS:standard',
+        ignore: 'pid,hostname',
+    },
+}];
+
+if (enableFileLogging) {
+    targets.push({
+        level: fileLogLevel,
+        target: 'pino/file',
+        options: { destination: destinationLogFile }
+    });
+}
 
 const logger = pino({
     level: logLevel,
     transport: {
-        targets: [
-            {
-                level: logLevel,
-                target: 'pino-pretty',
-                options: {
-                    colorize: true,
-                    translateTime: 'SYS:standard',
-                    ignore: 'pid,hostname',
-                },
-            },
-            // {
-            //     level: fileLogLever,
-            //     target: 'pino/file',
-            //     options: { destination: destinationLogFile }
-            // }
-        ]
+        targets: targets
     }
 });
 
