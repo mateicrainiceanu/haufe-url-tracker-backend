23b492c796b91ea0cc0b5575c729e159e5e0014b
mateicrainiceanu <matei.crainiceanu@gmail.com>
2025-05-20 11:51:29 +0300

    added validation on routes and /get form [teamid]

--- src/routes/dev/configDevRoutes.ts
+++ src/routes/dev/configDevRoutes.ts
@@ -1,6 +1,6 @@
 import {Router} from 'express';
 import validate from '../../utils/middleware/validate';
-import {body} from "express-validator";
+import {body, param} from "express-validator";
 import {validate as isUUID} from "uuid";
 import team from "../../utils/middleware/teamMiddleware";
 import auth from "../../utils/middleware/auth";
@@ -8,10 +8,9 @@ import DevKeysController from "../../controllers/DevKeysController";
 
 const configDevRoutes = Router();
 
-// configDevRoutes.get('/:teamId', (req, res) => {})
-
-configDevRoutes.post('/api-key', auth, team,
+configDevRoutes.post('/api-key',
     validate([body("teamId").custom(isUUID).withMessage("Team id is mandatory and has to be valid teamId")]),
+    auth, team,
     async (req, res) => {
         const {team} = req;
 
@@ -19,15 +18,18 @@ configDevRoutes.post('/api-key', auth, team,
         res.json({key});
     });
 
-configDevRoutes.get("/api-keys/:teamId", auth, team, (req, res) => {
-        // const {team} = req;
-        res.send("Not implemented")
-    }
-);
+configDevRoutes.get("/api-keys/:teamId",
+    validate([param("teamId").custom(isUUID).withMessage("Team id is mandatory and has to be valid teamId")]),
+    auth, team,
+    (req, res) => {
+        res.json({team: req.team, devKeys: req.team.devKeys})
+    });
 
-configDevRoutes.delete("/api-key/:keyId", auth, async (req, res) => {
-    await DevKeysController.deleteDevKey(req.params.keyId, req.user);
-    res.status(200).send("DevKey was deleted successfully");
-});
+configDevRoutes.delete("/api-key/:keyId",
+    validate([param("keyId").custom(isUUID).withMessage("Key id is mandatory and has to be valid")]),
+    auth, async (req, res) => {
+        await DevKeysController.deleteDevKey(req.params.keyId, req.user);
+        res.status(200).send("DevKey was deleted successfully");
+    });
 
 export default configDevRoutes;
